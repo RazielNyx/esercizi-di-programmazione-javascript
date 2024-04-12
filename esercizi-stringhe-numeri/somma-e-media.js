f229ea9481adfc263309165f76a18d9a51f18997
RazielNyx <82269720+RazielNyx@users.noreply.github.com>
2024-04-12 19:12:25 +0200

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -5,6 +5,11 @@
   Esempio:
     Input: a = 1, b = 2, c = 3, d = 4, e = 5
     Output: somma = 15, media = 3
-  
+
   http://www.imparareaprogrammare.it
 */
+
+let a = 27, b = 1, c = 9, d = 8, e = 7;
+let sum = a+b+c+d+e;
+
+console.log(`somma = ${sum}, media = ${sum/5}`)
