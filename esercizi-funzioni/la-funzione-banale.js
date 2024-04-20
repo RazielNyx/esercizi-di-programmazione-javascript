b8c281b438ea1a8e781ad12d9bc2264ce4794d9a
RazielNyx <166723228+RazielNyx@users.noreply.github.com>
2024-04-20 17:04:13 +0200

    Update la-funzione-banale.js

--- esercizi-funzioni/la-funzione-banale.js
+++ esercizi-funzioni/la-funzione-banale.js
@@ -13,6 +13,20 @@
     Input: n = 2, m = 2
     Output: TRUE
 
-    
+
   http://www.imparareaprogrammare.it
+*/
+
+let n = 2;
+let m = 3;
+
+if (n === m) {
+      console.log("TRUE")
+} else {
+    console.log("FALSE")
+};
+
+/*
+l'operatore Strict equality (===) indica un'uguaglianza stretta, e considera sempre come diversi i differenti tipi di variabile:
+una stringa non sarà mai uguale a un numero, quindi con l'input: n = 2, m = '2' l'output è FALSE; se avessi usato l'operatore Equality (==) con input: n = 2 e m = '2', l'output sarebbe stato TRUE).
 */
