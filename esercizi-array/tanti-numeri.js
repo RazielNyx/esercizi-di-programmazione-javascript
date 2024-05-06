240dcd111d6b9d57f7437009189880d0ec5422c3
RazielNyx <166723228+RazielNyx@users.noreply.github.com>
2024-05-06 14:56:40 +0200

    Update tanti-numeri.js

--- esercizi-array/tanti-numeri.js
+++ esercizi-array/tanti-numeri.js
@@ -12,3 +12,25 @@
 
   http://www.imparareaprogrammare.it
 */
+
+let a = [3, 5, 10, 2, 8];
+
+//ora sommo tutti i numeri nella variabile a con un loop
+let sum = 0;
+for (let i = 0; i < a.length; i++) {
+  sum += a[i]
+};
+
+//calcolo la media
+let media = sum/a.length;
+
+//filtro la variabile a prima coi numeri minori, poi con quelli maggiori della media
+const min = a.filter(function (number) {
+  return (number < media)
+});
+const max = a.filter(function (number) {
+  return (number > media)
+});
+
+
+console.log(`media = ${media}, valori minori = [${min}], valori maggiori = [${max}]`);
